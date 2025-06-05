071035d6ce38fdbac8f78bdab337c0234e8c297c
moliveri1618 <lastiada1@gmail.com>
2025-06-05 18:48:07 +0100

    d

--- api/routers/commesse.py
+++ api/routers/commesse.py
@@ -25,6 +25,10 @@ def read_commesse(db: Session = Depends(get_db)):
 
 @router.get("/odoo")
 def get_commesse_from_odoo(db: Session = Depends(get_db)):
+    
+    #test
+    return JSONResponse(content={"message": "Sync complete", "inserted": 1}, status_code=200)
+
         
     # Connect to the common service and authenticate
     models = client.ServerProxy(f'{SERVER_URL_ODOO}/xmlrpc/2/object')
