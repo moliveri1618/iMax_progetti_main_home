6741cef40f91238165a2d4481bc3e5ff255c0580
moliveri1618 <lastiada1@gmail.com>
2025-06-19 19:08:04 +0100

    T

--- api/main.py
+++ api/main.py
@@ -7,8 +7,7 @@ from contextlib import asynccontextmanager
 import asyncio
 
 if os.getenv("GITHUB_ACTIONS"):sys.path.append(os.path.dirname(__file__)) 
-from routers import commesse, vendite, tickets, workInProgress, savePDF
-#, rilievoMisure, collaudoFinale
+from routers import commesse, vendite, tickets, workInProgress, savePDF, rilievoMisure, collaudoFinale
 from dependecies import create_db_and_tables, verify_cognito_token
 
 
@@ -59,17 +58,17 @@ app.include_router(
 #     tags=["savePDF"]
 #     )
 
-# app.include_router(
-#     rilievoMisure.router, 
-#     prefix="/rilievoMisure", 
-#     tags=["rilievoMisure"]
-#     )
+app.include_router(
+    rilievoMisure.router, 
+    prefix="/rilievoMisure", 
+    tags=["rilievoMisure"]
+    )
 
-# app.include_router(
-#     collaudoFinale.router, 
-#     prefix="/collaudoFinale", 
-#     tags=["collaudoFinale"]
-#     )
+app.include_router(
+    collaudoFinale.router, 
+    prefix="/collaudoFinale", 
+    tags=["collaudoFinale"]
+    )
 
 
 @app.get("/")
