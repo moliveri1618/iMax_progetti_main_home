d8a38788476b0f68bb6337bd1bb97e0eb53f2a12
moliveri1618 <lastiada1@gmail.com>
2025-08-12 17:26:43 +0100

    d

--- api/models/iParametriDaInserire.py
+++ api/models/iParametriDaInserire.py
@@ -3,6 +3,7 @@ from typing import Optional
 
 class ParametriDaInserire(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
+    user_id: Optional[str] = Field(default=None, index=True)  
     mese: str = Field(index=True)  
     obiettivo_mensile: float = Field(default=0.0)  
     perc_premio_trimestrale: Optional[float] = Field(default=None)  
