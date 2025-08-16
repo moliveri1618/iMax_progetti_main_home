718fd1574e7e68630f6305e9ed65bff3d66d28c5
moliveri1618 <lastiada1@gmail.com>
2025-08-16 02:01:01 +0100

    d

--- api/schemas/iParametriDaInserire.py
+++ api/schemas/iParametriDaInserire.py
@@ -84,6 +84,27 @@ TEMPLATE_ROWS = [
     {"mese": "dicembre",  "obiettivo_mensile": 1000, "perc_premio_trimestrale": 0.5,  "perc_premio_annuale": None, "valore_limite": 60,  "perc_100_budget": -3},
 ]
 
+###########################
+#### TEST FROM FASTAPI ####
+###########################
+'''
+[
+  {"mese":"gennaio","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":0.14,"valore_limite":140,"perc_100_budget":1700},
+  {"mese":"febbraio","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":null,"valore_limite":999,"perc_100_budget":8},
+  {"mese":"marzo","obiettivo_mensile":1000,"perc_premio_trimestrale":0.5,"perc_premio_annuale":null,"valore_limite":110,"perc_100_budget":5},
+  {"mese":"aprile","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":null,"valore_limite":100,"perc_100_budget":3.5},
+  {"mese":"maggio","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":null,"valore_limite":95,"perc_100_budget":3},
+  {"mese":"giugno","obiettivo_mensile":1000,"perc_premio_trimestrale":0.5,"perc_premio_annuale":null,"valore_limite":90,"perc_100_budget":2.5},
+  {"mese":"luglio","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":null,"valore_limite":85,"perc_100_budget":2},
+  {"mese":"agosto","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":null,"valore_limite":80,"perc_100_budget":1.5},
+  {"mese":"settembre","obiettivo_mensile":1000,"perc_premio_trimestrale":0.5,"perc_premio_annuale":null,"valore_limite":75,"perc_100_budget":1},
+  {"mese":"ottobre","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":null,"valore_limite":70,"perc_100_budget":0.5},
+  {"mese":"novembre","obiettivo_mensile":1000,"perc_premio_trimestrale":null,"perc_premio_annuale":null,"valore_limite":65,"perc_100_budget":-1},
+  {"mese":"dicembre","obiettivo_mensile":1000,"perc_premio_trimestrale":0.5,"perc_premio_annuale":null,"valore_limite":60,"perc_100_budget":-3}
+]
+'''
+
+
 
 MONTHS = {r["mese"] for r in TEMPLATE_ROWS}
 MONTH_ORDER = {m: i for i, m in enumerate([r["mese"] for r in TEMPLATE_ROWS])}
