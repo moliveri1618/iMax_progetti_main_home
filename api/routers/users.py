5c7749f38d84d3d5c13235a3cbbadefac413a0a9
moliveri1618 <lastiada1@gmail.com>
2025-12-19 20:34:27 +0000

    d

--- api/routers/users.py
+++ api/routers/users.py
@@ -173,6 +173,7 @@ def sync_user_from_odoo(db: Session = Depends(get_db)):
                 "Attacchi": True,
                 "Montaggio a Bordo": True,
                 "Filo guidatura": True,
+                "Collaudo Finale": True,
             },
             home={
                 "Rilievo Misure": True,
@@ -184,6 +185,7 @@ def sync_user_from_odoo(db: Session = Depends(get_db)):
                 "TAGLIO TELAI": True,
                 "POSA SERRAMENTO": True,
                 "RIVESTIMENTO INTERNO": True,
+                "Collaudo Finale": True,
             },
         )
         db.add(entity)
