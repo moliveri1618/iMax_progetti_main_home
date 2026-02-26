e646d28197ca1d0c5d630d2141ac26976520d575
moliveri1618 <lastiada1@gmail.com>
2026-02-27 00:24:12 +0100

    a

--- api/routers/tickets.py
+++ api/routers/tickets.py
@@ -6,6 +6,7 @@ from xmlrpc import client
 from fastapi.responses import JSONResponse
 from fastapi import Query
 import httpx
+import pprint
 
 if os.getenv("GITHUB_ACTIONS"):sys.path.append(os.path.dirname(__file__))
 from models.tickets import HelpdeskTicket
@@ -111,63 +112,99 @@ def update_ticket(ticket_id: int, payload: TicketUpdate, db: Session = Depends(g
 def fetch_helpdesk_tickets_v2(db: Session = Depends(get_db)):
     
     try:
-        
-        # 1. Search for tickets (no domain = fetch all)
-        ticket_ids = rpc_call(
-            "ticket.helpdesk",
-            "search",
-            [[]]
-        )
-        print(ticket_ids)
-        
-        # 2. Read ticket data
-        tickets = rpc_call(
-            "ticket.helpdesk",
-            "read",
-            [ticket_ids],
-            {
-                "fields": [
-                    "name",         # ticket_ref & name
-                    "priority",     # priority
-                    "customer_id",  # customer
-                    "subject",      #
-                    "stage_id",     # stage
-                    "tags_ids",     # type
-                    "create_date"   # created
-                ]
-            }
-        )
-        print(tickets)
 
-        inserted = 0
-        skipped = 0
+        tickets = rpc_call(
+            "helpdesk.ticket",
+            "search_read",
+            [[
+                ("active", "=", True),
+                ("team_id", "in", [5, 6]),
+                ("stage_id.fold", "=", False),
+            ]],
+            {"fields": [
+                "id", 
+                "number", 
+                "name", 
+                "priority", 
+                "stage_id", 
+                "tag_ids", 
+                "create_date", 
+                "team_id",
+                "partner_id",
+                "partner_email",
+                "user_id"
+            ]}
+        )
+
+        # 1) find user email
+        user_ids = sorted({t["user_id"][0] for t in tickets if t.get("user_id")})
+        if user_ids:
+            users = rpc_call(
+                "res.users",
+                "read",
+                [user_ids],
+                {"fields": ["id", "name", "login", "partner_id"]}
+            )
+            user_by_id = {u["id"]: u for u in users}
+        print(user_ids)
+        print(user_by_id)
+
+        # create tickets
+        nautica, home = [], []
         for t in tickets:
 
+            # customer
+            name = t["partner_id"][1] if t.get("partner_id") else "Unknown"
+            email = t.get("partner_email") or "N/A"
+            t["customer"] = f"{name}, {email}"
 
-            # ✅ exists check by ticket_ref
+            # assigned to
+            user_id = t.get("user_id")
+            if user_id:
+                u = user_by_id.get(user_id[0], {})
+                t["assigned_to"] = u.get("name") + ", " + u.get("login")
+
+            # split nautica & home
+            tid = t["team_id"][0] if t.get("team_id") else None
+            if tid == 5: 
+                nautica.append(t)
+            elif tid == 6:
+                home.append(t)
+
+        print("NAUTICA:", len(nautica))
+        pprint.pprint(nautica)
+        print("\nHOME:", len(home))
+        pprint.pprint(home)
+
+        # find existing refs 
+        incoming_refs = [str(t.get("number") or "") for t in home]
+        existing_refs = set()
+        if incoming_refs:
+            stmt = select(HelpdeskTicket.ticket_ref).where(HelpdeskTicket.ticket_ref.in_(incoming_refs))
+            existing_refs = set(db.exec(stmt).all())
+        print('incoming_refs', incoming_refs)
+        print('existing refs', existing_refs)
+
+        inserted = 0
+        for t in home:
             ticket_ref = str(t.get("name") or "")
-            if ticket_ref:
-                exists_stmt = select(HelpdeskTicket.id).where(HelpdeskTicket.ticket_ref == ticket_ref)
-                exists = db.exec(exists_stmt).first()
-                if exists:
-                    skipped += 1
-                    continue
+            if not ticket_ref:
+                continue
 
+            if ticket_ref in existing_refs:
+                skipped += 1
+                continue
 
             row = HelpdeskTicket(  
-                ticket_ref=str(t.get("name") or ""),
+                ticket_ref=str(t.get("number") or ""),
                 name=(t.get("name") or ""),
                 priority=str(t.get("priority") or ""),
-                customer=(t["customer_id"][1] if t.get("customer_id") else ""),
-                assigned_to="Unassigned",
+                customer=(t.get("customer") or ""),
+                assigned_to=(t.get("assigned_to") or ""),
                 stage=(t["stage_id"][1] if t.get("stage_id") else ""),
                 team="N/A",
                 created=(t.get("create_date") or ""),
-                type=(
-                    "nautica" if 2 in (t.get("tags_ids") or [])
-                    else "home" if 1 in (t.get("tags_ids") or [])
-                    else ""
-                ),                
+                type=('home'),                
                 completato=False,
             )
             db.add(row)
